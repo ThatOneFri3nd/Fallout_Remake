4f0280c911a216d10a28d4a61ba079e8be57dbe5
ShinySSB <38051736+ShinySSB@users.noreply.github.com>
2024-06-05 00:49:36 +0200

    implemented basic camera controls

--- scripts/core/main/camera_control.gd
+++ scripts/core/main/camera_control.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export var mouse_sens: float
 @export var camera_acceleration: float
 @export var max_camera_speed: float
+var camera_speed: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +11,7 @@ func _ready():
 
 
 func _input(event):
-	var forward = self.transform.basis.z * camera_acceleration
-	
+	var fixed_delta: float = get_physics_process_delta_time()
 	if Input.is_action_pressed("camera_rotate"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		if event is InputEventMouseMotion:
@@ -20,3 +20,16 @@ func _input(event):
 	
 	if Input.is_action_just_released("camera_rotate"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if Input.is_action_pressed("camera_forward"):
+		camera_speed += camera_acceleration * fixed_delta
+		position.z -= camera_speed * fixed_delta
+	if Input.is_action_pressed("camera_backward"):
+		camera_speed += camera_acceleration * fixed_delta
+		position.z += camera_speed * fixed_delta
+	if Input.is_action_pressed("camera_left"):
+		camera_speed += camera_acceleration * fixed_delta
+		position.x -= camera_speed * fixed_delta
+	if Input.is_action_pressed("camera_right"):
+		camera_speed += camera_acceleration * fixed_delta
+		position.x += camera_speed * fixed_delta

46cd3d4e7638b9fb8745a28d1f669222599013c0
ShinySSB <38051736+ShinySSB@users.noreply.github.com>
2024-06-04 13:22:40 +0200

    worked on palayer position and spawn

---
+++ scenes/main/Main Scene.gd
@@ -0,0 +1,35 @@
+extends Control
+
+@onready var hud : Control = $HUD
+@onready var menu : Control = $Menu
+@onready var CurrentLevel = $CurrentLevel
+var level_instance : Node3D
+var player_spawn_position: Vector3
+
+signal level_loaded
+
+func unload_level():
+	if (is_instance_valid(level_instance)):
+		level_instance.queue_free()
+	level_instance = null
+
+func load_level(level_name: String):
+	unload_level()
+	var level_path := "res://scenes/maps/%s.tscn" % level_name
+	var level_resource := load(level_path)
+	var player_resource := load("res://scenes/player/Player.tscn")
+	if level_resource:
+		level_instance = level_resource.instantiate()
+		CurrentLevel.add_child(level_instance)
+		emit_signal("level_loaded")
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scripts/core/player/player_handler.gd
+++ scripts/core/player/player_handler.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@onready var player: Node3D = get_node("Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_main_scene_level_loaded():
+	var player_position: Vector3 = get_node("PlayerSpawn").transform
+	player.position = player_position

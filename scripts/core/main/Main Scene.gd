d7ef11ca99a3f20b968152ec2b63e67d26bafa97
ShinySSB <lilysandberg98@gmail.com>
2024-06-10 01:26:39 +0200

    player movement done

--- scripts/core/main/Main Scene.gd
+++ scripts/core/main/Main Scene.gd
@@ -23,12 +23,9 @@ func load_level(level_name: String):
 		CurrentLevel.add_child(level_instance)
 		emit_signal("level_loaded")
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
--- scripts/core/player/player_handler.gd
+++ scripts/core/player/player_handler.gd
@@ -1,7 +1,9 @@
-extends Node3D
+extends CharacterBody3D
 
 @onready var player : Node3D = self
 @onready var navigation_agent := $NavigationAgent3D
+@export var movement_speed: float = 10
+@export var ray_length: float = 10000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,20 +11,29 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func _process(delta):
+	if navigation_agent.is_navigation_finished():
+		return
+	
+	move_to_point(delta, movement_speed)
+
+func move_to_point(delta, speed):
+	var target_position = navigation_agent.get_next_path_position()
+	var direction = global_position.direction_to(target_position)
+	
+	velocity = direction * speed
+	move_and_slide()
 
 func _input(event):
 	if Input.is_action_just_pressed("interact"):
-		var target_position = raycast()
-		move_player(target_position)
+		var interact_result = raycast()
+		move_player(interact_result)
 
 func raycast():
 	var camera: Camera3D = get_node("../Camera/Camera3D")
 	if camera == null:
 		printerr("Error! Can't find Camera3D!")
-	var interact_pos = get_viewport().get_interact_position()
-	var ray_length = 100
+	var interact_pos = get_viewport().get_mouse_position()
 	var from = camera.project_ray_origin(interact_pos)
 	var to = from + camera.project_ray_normal(interact_pos) * ray_length
 	var space = get_world_3d().direct_space_state
@@ -32,11 +43,11 @@ func raycast():
 	ray_query.to = to
 	
 	var result = space.intersect_ray(ray_query)
-	print(result)
 	return result
 
-func move_player(target_position):
-	pass
+func move_player(interact_result):
+	navigation_agent.target_position = interact_result.position
+	print(navigation_agent.target_position)
 
 func _on_level_loaded():
 	var player_spawn: Vector3 = get_node("PlayerSpawn").transform
